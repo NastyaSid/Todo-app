2179978b4c75316dffde13aad98e69503b6b9585
Nastya <anastasiia.y.sitnikova@gmail.com>
2023-04-14 15:59:00 +0200

    made changes after review

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import './App.scss';
 import TodoListItem from './components/TodoListItem';
 
@@ -17,10 +17,6 @@ function App() {
   const [tasks, setTasks] = useState(defaultTasks);
   const [currentFilter, setCurrentFilter] = useState(ALL);
 
-  useEffect(() => {
-    console.log(tasks)
-  }, [tasks])
-
   const addTask = (value) => {
     const newTask = {value, id: Date.now(), isDone: false}
     setTasks([...tasks, newTask])
@@ -36,10 +32,10 @@ function App() {
     inputField.value = '';
   }
 
-  const onDelete = (id) => {
-    const currentTasks = tasks.filter(task => id !== task.id)
-    setTasks(currentTasks);
-  }
+  // const onDelete = (id) => {
+  //   const currentTasks = tasks.filter(task => id !== task.id)
+  //   setTasks(currentTasks);
+  // }
 
   const onToggle = (id) => {
     const checkedTask = tasks.map((task) => {
@@ -59,31 +55,6 @@ function App() {
   const filteredTasks = useMemo(filterByStatus, [currentFilter, tasks]);
   const subheader = `${filteredTasks.length} tasks remaining`;
 
-  // const handleEdit = (id) => {
-  //   tasks.map((task) => {
-  //     if (id === task.id) {
-  //       const inputField = document.querySelector('.edit-input');
-  //       inputField.value = task.value;
-  //       document.querySelector('.edit-mode').classList.toggle('active');
-  //       document.querySelector('.reading-mode').classList.toggle('hidden');
-  //     }
-  //     return task;
-  //   })
-  // }
-
-  // const handleSave = (id) => {
-  //   const changedTask = tasks.map((task) => {
-  //     if (task.id === id) {
-  //       document.querySelector('.edit-mode').classList.remove('active');
-  //       document.querySelector('.reading-mode').classList.remove('hidden');
-  //       return {...task, value: document.querySelector('.edit-input').value}
-  //     }
-  //     console.log(task)
-  //     return task;
-  //   })
-  //   setTasks(changedTask);
-  // }
-
   return (
     <div className="todoapp-container">
       <div className='input-section'>
@@ -101,7 +72,7 @@ function App() {
         <h2 className='subheader'>{subheader}</h2>
         <ul className='todo-list'>
           {filteredTasks.map(({value, id, isDone}) => {
-            return <TodoListItem value={value} id={id} key={id} isDone={isDone} tasks={tasks} setTasks={setTasks} onDelete={onDelete} onToggle={onToggle} />
+            return <TodoListItem value={value} id={id} key={id} isDone={isDone} tasks={tasks} setTasks={setTasks} onToggle={onToggle} />
           })}
         </ul>
       </div>
--- src/components/TodoListItem.jsx
+++ src/components/TodoListItem.jsx
@@ -1,32 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-export default function TodoListItem({ value, id, isDone, tasks, setTasks, onDelete, onToggle }) {
+export default function TodoListItem({ value, id, isDone, tasks, setTasks, onToggle }) {
 
-  const changeStyles = (e) => {
-    const elem = e.currentTarget.closest('.todo-list__item');
-    elem.querySelector('.edit-mode').classList.toggle('active');
-    elem.querySelector('.reading-mode').classList.toggle('hidden');
+  const [isEditing, setIsEditing] = useState(false);
+  const inputElement = useRef();
+
+  const manageInput = () => {
+    inputElement.current.value = value;
+    inputElement.current.focus();
   }
 
-  const handleEdit = (id, e) => {
-    tasks.map((task) => {
-      if (task.id === id) {
-        changeStyles(e);
-        const elem = e.currentTarget.closest('.todo-list__item');
-        const inputField = elem.querySelector('.edit-input');
-        inputField.value = task.value;
-      }
-      return task;
-    })
+  const onDelete = (id) => {
+    const currentTasks = tasks.filter(task => id !== task.id)
+    setTasks(currentTasks);
+  }
+
+  const handleEdit = () => {
+    setIsEditing(true);
+
+    setTimeout(manageInput, 0);
   }
 
   const handleSave = (id, e) => {
     const changedTask = tasks.map((task) => {
       if (task.id === id) {
-        changeStyles(e);
         const elem = e.currentTarget.closest('.todo-list__item');
-        return {...task, value: elem.querySelector('.edit-input').value}
+        return { ...task, value: elem.querySelector('.edit-input').value }
       }
+      setIsEditing(false)
       return task;
     })
     setTasks(changedTask);
@@ -34,23 +35,27 @@ export default function TodoListItem({ value, id, isDone, tasks, setTasks, onDel
 
   return (
     <li className='todo-list__item'>
-      <div className="reading-mode">
-        <div className='todo-list__name'>
-          <input id={id} type="checkbox" checked={isDone} className='custom-checkbox' onChange={() => onToggle(id)}/>
-          <label htmlFor={id}>{value}</label>
+      {!isEditing && (
+        <div className="reading-mode">
+          <div className='todo-list__name'>
+            <input id={id} type="checkbox" checked={isDone} className='custom-checkbox' onChange={() => onToggle(id)} />
+            <label htmlFor={id}>{value}</label>
+          </div>
+          <div className='todo-list__buttons'>
+            <button className='btn edit-btn' onClick={handleEdit}>Edit</button>
+            <button className='btn' onClick={() => onDelete(id)}>Delete</button>
+          </div>
         </div>
-        <div className='todo-list__buttons'>
-          <button className='btn edit-btn' onClick={(e) => handleEdit(id, e)}>Edit</button>
-          <button className='btn' onClick={() => onDelete(id)}>Delete</button>
+      )}
+      {isEditing && (
+        <div className="edit-mode">
+          <input ref={(ref) => inputElement.current = ref } className="edit-input" type='text' />
+          <div className='todo-list__buttons'>
+            <button className='btn cancel-btn' onClick={() => setIsEditing(false)}>Cancel</button>
+            <button className='btn' onClick={(e) => handleSave(id, e)}>Save</button>
+          </div>
         </div>
-      </div>
-      <div className="edit-mode">
-        <input className="edit-input" type='text' />
-        <div className='todo-list__buttons'>
-          <button className='btn cancel-btn' onClick={(e) => handleEdit(id, e)}>Cancel</button>
-          <button className='btn' onClick={(e) => handleSave(id, e)}>Save</button>
-        </div>
-      </div>
+      )}
     </li>
   );
 }
